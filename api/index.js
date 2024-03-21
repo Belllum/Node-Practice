382845621604812b6e59730e9550ca87d3de2c35
CWDgabriel <jose.gabriel@clockworkdelivery.com>
2024-03-21 21:27:35 +0800

    update

--- api/index.js
+++ api/index.js
@@ -9,10 +9,45 @@ app.get("/", (req, res) => {
   res.send("hello testing");
 });
 
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find(req.body);
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Update
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    const newProduct = await Product.findById(id);
+
+    res.status(200).json(newProduct);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
